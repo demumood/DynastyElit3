177fbc06278b38a5216c8d510ae6af9d08784614
Mahmoud Audu <demumood@gmail.com>
2020-09-08 12:07:37 -0400

    Add project files.

---
+++ DynastyElit3/Controllers/TailorController.cs
@@ -0,0 +1,45 @@
+﻿
+namespace DynastyElit3.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Models.ViewModels;
+
+    public class TailorController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<TailorViewModel> data = new List<TailorViewModel>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                TailorViewModel addData = new TailorViewModel()
+                {
+                    ID = i,
+                    Name = "Test User " + i,
+                    isActive = true
+                };
+                data.Add(addData);
+            }
+
+            return View(data);
+        }
+
+        public IActionResult AddEdit()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddEdit([Bind(include: "Id,Name,isActive")]TailorViewModel data)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ DynastyElit3/Models/ViewModels/TailorViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace DynastyElit3.Models.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class TailorViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool isActive { get; set; }
+    }
+}
